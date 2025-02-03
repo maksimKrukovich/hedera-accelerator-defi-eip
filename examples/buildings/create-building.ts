b51a256af14bfd80cbb324f4233a96dff78be3de
nadineloepfe <nadine.loepfe@swirldslabs.com>
2025-02-03 16:10:48 +0100

    Create building fix (#71)
    Signed-off-by: nadine.loepfe <nadine.loepfe@swirldslabs.com>

--- examples/buildings/create-building.ts
+++ examples/buildings/create-building.ts
@@ -1,19 +1,26 @@
 import { ethers } from 'hardhat';
-import Deployments from  '../../data/deployments/chain-296.json';
+import Deployments from '../../data/deployments/chain-296.json';
 
 async function createBuilding() {
+  const buildingFactory = await ethers.getContractAt(
+    "BuildingFactory",
+    Deployments.factories.BuildingFactory
+  );
 
-  const buildingFactory = await ethers.getContractAt('BuildingFactory', Deployments.factories.BuildingFactory);
-
-  const tokenURI = "ipfs://bafkreifuy6zkjpyqu5ygirxhejoryt6i4orzjynn6fawbzsuzofpdgqscq"; // URl of building metadata, it will be used to mint new building NFT
+  const tokenURI = "ipfs://bafkreifuy6zkjpyqu5ygirxhejoryt6i4orzjynn6fawbzsuzofpdgqscq"; // URl of building metadata, it will be used to mint new building NFT 
   const tx = await buildingFactory.newBuilding(tokenURI);
   await tx.wait();
 
-  const [[buildingAddress]] = await buildingFactory.getBuildingList();
-  const buildingDetails = await buildingFactory.getBuildingDetails(buildingAddress);
+  const buildingList = await buildingFactory.getBuildingList();
 
-  console.log({buildingDetails});
+  const newlyCreated = buildingList[buildingList.length - 1];
+
+  console.log("New building info:", newlyCreated);
+  console.log("New building address:", newlyCreated.addr);
 }
 
 createBuilding()
-  .catch(console.error);
+  .catch((err) => {
+    console.error(err);
+    process.exitCode = 1;
+  });
